3cc3ecfc34693878d46e10f0204cfff5bbae5c0c
jimmyy200 <jimmy322@my.yorku.ca>
2025-11-19 14:05:19 -0500

    Modified Node class
    Data type is Material instead of int

--- Jimmy, Muhammad, Nathan, Harris - Lab5 OOP Implementation/src/main/java/com/university/bookstore/Lab5Implementations/BSTsAndTraversals.java
+++ Jimmy, Muhammad, Nathan, Harris - Lab5 OOP Implementation/src/main/java/com/university/bookstore/Lab5Implementations/BSTsAndTraversals.java
@@ -1,4 +1,18 @@
 package com.university.bookstore.Lab5Implementations;
 
+import java.io.*;
+import com.university.bookstore.model.Material;
+
+class Node{
+    Material data;
+    Node left;
+    Node right;
+    Node(Material data){
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+}
+
 public class BSTsAndTraversals {
 }
