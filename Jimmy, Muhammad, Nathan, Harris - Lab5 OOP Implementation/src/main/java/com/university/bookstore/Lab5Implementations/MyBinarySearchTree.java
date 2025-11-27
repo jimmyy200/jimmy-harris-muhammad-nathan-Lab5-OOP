3a41dd8a2fc4e5d3cbd25d9ccd7e14846ce91a0c
jimmyy200 <jimmy322@my.yorku.ca>
2025-11-27 13:37:39 -0500

    Added comments

--- Jimmy, Muhammad, Nathan, Harris - Lab5 OOP Implementation/src/main/java/com/university/bookstore/Lab5Implementations/MyBinarySearchTree.java
+++ Jimmy, Muhammad, Nathan, Harris - Lab5 OOP Implementation/src/main/java/com/university/bookstore/Lab5Implementations/MyBinarySearchTree.java
@@ -7,18 +7,33 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
 
     private TreeNode<T> root;
     private int size;
+
+    /**
+     * Constructs a new binary search tree
+     */
     public MyBinarySearchTree(){
         root = null;
         size = 0;
     }
 
+    /**
+     * Calls the other insert function
+     * @param key any data type that uses generics
+     */
     public void insert(T key){
         root = insert(root, key);
     }
 
+    /**
+     * Inserts object into the BST
+     * @param node starting point (root)
+     * @param key object/data type to be inserted
+     * @return root
+     */
     @Override
     public TreeNode<T> insert(TreeNode<T> node, T key) {
         if (node == null) {
+            size++;
             return new TreeNode<>(key);
         }
 
@@ -31,9 +46,21 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
         return node;
     }
 
+    /**
+     * Calls the other contains function
+     * @param key any data type that uses generics
+     * @return true if it contains the key, false if it does not contain the key
+     */
     public boolean contains(T key){
         return contains(root, key);
     }
+
+    /**
+     * Searches the BST to see if it contains the given key
+     * @param node starting point of the BST (root)
+     * @param key generic object/data type to be searched for
+     * @return true if it contains the key, false if it does not contain the key
+     */
     @Override
     public boolean contains(TreeNode<T> node, T key) {
         if (node == null) {
@@ -49,15 +76,32 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
         }
     }
 
+    /**
+     * Helper class for delete method to find successor in BST
+     * @param node any data type/object that is generic
+     * @return
+     */
     private TreeNode<T> findMinimum(TreeNode<T> node){
         while (node.left != null){
             node = node.left;
         }
         return node;
     }
+
+    /**
+     * Calls other delete function without needing root
+     * @param key generic data type to be deleted
+     */
     public void delete(T key){
         delete(root, key);
     }
+
+    /**
+     * Deletes a specified key
+     * @param node starting point(root)
+     * @param key generic data type to be deleted
+     * @return starting point(root)
+     */
     @Override
     public TreeNode<T> delete(TreeNode<T> node, T key) {
         if (node == null) {
@@ -70,12 +114,15 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
             node.right = delete(node.right, key);
         } else {
             if (node.left == null && node.right == null){
+                size--;
                 return null;
             }
             if (node.left == null){
+                size--;
                 return node.right;
             }
             if (node.right == null){
+                size--;
                 return node.left;
             }
             TreeNode<T> successor = findMinimum(node.right);
@@ -85,9 +132,17 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
         return node;
     }
 
+    /**
+     * Calls the other inOrderTraversal function without having the root
+     */
     public void inOrderTraversal(){
         inOrderTraversal(root);
     }
+
+    /**
+     * inOrderTraversal of the BST (left, root, right)
+     * @param node root
+     */
     @Override
     public void inOrderTraversal(TreeNode<T> node) {
         if (node != null){
@@ -97,9 +152,17 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
         }
     }
 
+    /**
+     * Calls the other preOrderTraversal function without having the root
+     */
     public void preOrderTraversal(){
         preOrderTraversal(root);
     }
+
+    /**
+     * preOrderTraversal of the BST (root, left, right)
+     * @param node root
+     */
     @Override
     public void preOrderTraversal(TreeNode<T> node) {
         if (node != null){
@@ -109,9 +172,17 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
         }
     }
 
+    /**
+     * Calls the other postOrderTraversal of the BST without having the root
+     */
     public void postOrderTraversal(){
         postOrderTraversal(root);
     }
+
+    /**
+     * postOrderTraversal of the BST (left, right, root)
+     * @param node root
+     */
     @Override
     public void postOrderTraversal(TreeNode<T> node) {
         if (node != null){
@@ -121,9 +192,17 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
         }
     }
 
+    /**
+     * Calls the other levelOrderTraversal without having the root
+     */
     public void levelOrderTraversal(){
         levelOrderTraversal(root);
     }
+
+    /**
+     * Level by level traversal (left to right)
+     * @param node root
+     */
     @Override
     public void levelOrderTraversal(TreeNode<T> node) {
         if (node == null) return;
@@ -140,9 +219,19 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
         }
     }
 
+    /**
+     * Calls the other height function without having the root
+     * @return height
+     */
     public int height(){
         return height(root);
     }
+
+    /**
+     * Gets the height of the BST
+     * @param node root
+     * @return height
+     */
     @Override
     public int height(TreeNode<T> node) {
         if (node == null) return -1;
@@ -153,11 +242,19 @@ public class MyBinarySearchTree <T extends Comparable<T>> implements BinarySearc
         return 1 + Math.max(height(node.left), height(node.right));
     }
 
+    /**
+     * Gets the size of the BST
+     * @return size
+     */
     @Override
     public int size() {
         return size;
     }
 
+    /**
+     * Checks if the BST is empty
+     * @return true if BST is empty, false if BST is not empty
+     */
     @Override
     public boolean isEmpty() {
         return size == 0;
