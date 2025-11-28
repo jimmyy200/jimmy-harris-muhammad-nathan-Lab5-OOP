42ff6bf55cac2ca49e63ee63887593be99e6567d
nathanc5York <nathanc5@my.yorku.ca>
2025-11-27 21:08:06 -0500

    Added the javadoc comments

--- Jimmy, Muhammad, Nathan, Harris - Lab5 OOP Implementation/src/main/java/com/university/bookstore/Lab5Implementations/MaterialLinkedList.java
+++ Jimmy, Muhammad, Nathan, Harris - Lab5 OOP Implementation/src/main/java/com/university/bookstore/Lab5Implementations/MaterialLinkedList.java
@@ -5,30 +5,48 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
     private Node<T> tail;
     private int size;
 
+    /** 
+     * Returns the size of the DoublyLinkedList
+     * @return size
+     */
     @Override
     public int size(){
         return size;
     }
     
+    /** 
+     * Constructs a Doubly Linked List if no arguments are provided
+     */
     public MaterialLinkedList(){
         this.head = null;
         this.tail = null;
         size = 0;
     }
 
+    /** 
+     * Constructs a Doubly Linked List with a Node if provided
+     * @param first Node inputted as an argument to the constructor
+     */
     public MaterialLinkedList(Node first){
         this.head = first;
         this.tail = first;
         size = 1;
     }
 
+    /**
+     * Constructs a Doubly Linked List with a data type if provided
+     * @param data  generic T data that accepts any type
+     */
     public MaterialLinkedList(T data){ 
         Node newNode = new Node(data);
         this.head = newNode;
         this.tail = newNode;
         size = 1;
     }
-
+    /**
+     * Will turn the input into a node and then add it to the head of the linked list
+     * @param data generic T data that accepts any type
+     */
     @Override
     public void addFirst(T data){
         Node<T> newNode = new Node<>(data);
@@ -46,6 +64,10 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         size++;
     }
 
+    /**
+     * Inserts a node into the head of the linked list
+     * @param newNode Input Node
+     */
     @Override
     public void addFirst(Node<T> newNode){
         if (head != null){
@@ -62,6 +84,10 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         size++;
     }
 
+    /**
+     * turn the data into a node and insert it at the tail
+     * @param data generic T data that accepts any type
+     */
     @Override
     public void addLast(T data){
         Node<T> newNode = new Node<>(data);
@@ -78,7 +104,10 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         }
         size++;
     }
-
+    /**
+     * Inserts a node into the tail of the linked list
+     * @param newNode Input Node
+     */
     @Override
     public void addLast(Node<T> newNode){
         if (tail != null){
@@ -95,6 +124,11 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         size++;
     }
 
+    /**
+     * Given an valid index, the function will turn the data into a node and insert it at the index
+     * @param index index value has to be less than size and greater than 0
+     * @param data generic T data
+     */
     @Override
     public void insertAt(int index, T data){
         Node<T> newNode = new Node<>(data);
@@ -119,7 +153,11 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         }
         size++;
     }
-
+    /**
+     * Given a valid index, the function will insert a node at the index
+     * @param index must be a valid index
+     * @param newNode Input Node
+     */
     @Override
     public void insertAt(int index, Node newNode){
         if (index > size || index < 0){
@@ -143,7 +181,10 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         }
         size++;
     }
-
+    /**
+     * Removes the first Node in the linked list, and returns the value stored within it
+     * @return data within the Node
+     */
     @Override
     public T removeFirst(){
         if (head == null){
@@ -165,6 +206,10 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         return returnValue;
     }
 
+    /**
+     * Removes the last node in the linked list, and returns the data stored within it
+     * @return data stored within Node
+     */
     @Override
     public T removeLast(){
         if (tail == null){
@@ -186,6 +231,11 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         return returnValue;
     }
 
+    /**
+     * Given a valid index, the function will remove the node at the index, and return the data stored within it
+     * @param index
+     * @return data within Node
+     */
     @Override
     public T removeAt(int index){
         if (index == 0){
@@ -212,6 +262,10 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         return returnValue;
     }
 
+    /**
+     * returns the data within the first node
+     * @return Head Node Data
+     */
     @Override
     public T getFirst(){
         if (head == null){
@@ -222,6 +276,10 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         }
     }
 
+    /**
+     * Returns the data within the tail Node
+     * @return Tail Node Data
+     */
     @Override
     public T getLast(){
         if (tail == null){
@@ -232,6 +290,11 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         }
     }
 
+    /**
+     * given a valid index, the function will return the data stored within the Node at the index
+     * @param index valid Index
+     * @return Node[index] Data
+     */
     @Override
     public T getAt(int index){
         if (index >= size){
@@ -249,6 +312,10 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         }
     }
 
+    /**
+     * checks the size value and the value of head and tail node to see if linked list is empty
+     * @return True or False
+     */
     @Override
     public boolean isEmpty(){
         if (size == 0 && head == null && tail == null){
@@ -259,6 +326,9 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         }
     }
 
+    /**
+     * Goes over each node in the linked list and clears its value to set it up for garbage collection
+     */
     @Override
     public void clear(){
         Node crNode = head;
@@ -274,6 +344,11 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         size = 0;
     }
     
+    /**
+     * Iterates through the linked list to see if a Node containing data exists, and if so returns true or false
+     * @param data check if data is in the linked list
+     * @return True or False
+     */
     @Override
     public boolean contains(T data){
         Node crNode = head;
@@ -286,6 +361,11 @@ public class MaterialLinkedList<T> implements DoublyLinkedList<T>{
         return false;
     }
 
+    /**
+     * iterates through the linked list, and if a node contains data, return the index of that node
+     * @param data check if data is in linked list
+     * @return index of Node that contains data
+     */
     @Override
     public int indexOf(T data){
         Node crNode = head;
